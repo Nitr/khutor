99dc20803848ee075f51c0061cdbb7d8e7d066ed
Evgeniy Stepanov <anty1989@mail.ru>
2011-12-23 11:13:00 +0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Khutor::Application.config.secret_token = 'e7c5ec13d35db738053c1992e744409d1df1bcdb9628666e514aa1e152ec5e05ca74eed268eda486d5f6f82ebf297b450274acf3553414c5a9a547c5cecbf5a4'
