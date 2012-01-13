e18dbfdb5552b3e48e68d9b2108f471232462a58
Evgeniy Stepanov <anty1989@mail.ru>
2012-01-13 12:00:02 +0400

    Добавил front-end

---
+++ public/javascripts/models/models.js
@@ -0,0 +1,10 @@
+	var trackerModel = Backbone.Model.extend({
+		urlRoot: 'http://0.0.0.0:3000/model'
+	});
+	var traker = Backbone.Model.extend({
+		urlRoot: 'http://0.0.0.0:3000/main'
+	});
+	var trackerData = Backbone.Model.extend({
+		urlRoot: 'http://0.0.0.0:3000/tracker'
+	});
+
